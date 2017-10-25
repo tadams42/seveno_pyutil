3037ba14d5b46bdb791db87c787fd3810e6d9c08
Tomislav Adamic <tomislav.adamic@gmail.com>
2017-10-25 13:33:55 +0200

    Added backports of Python 3.4 `min` and `max`.

--- src/seveno_pyutil/__init__.py
+++ src/seveno_pyutil/__init__.py
@@ -1,9 +1,17 @@
 __version__ = '0.2.1'
 
 import logging
+import sys
 
 from . import (datetime_utilities, dict_utilities, file_utilities,
                logging_utilities, metaprogramming_helpers, model_utilities,
                os_utilities, string_utilities)
+
+if sys.version_info < (3, 4, 0):
+    from .compatibility import py34_min as min
+    from .compatibility import py34_max as max
+else:
+    min = min
+    max = max
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
