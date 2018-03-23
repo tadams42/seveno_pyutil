5b1029d330832058055cee051ab6ef900fb52de4
Tomislav Adamic <tomislav.adamic@gmail.com>
2018-03-23 14:35:17 +0100

    Bump version: 0.2.9 → 0.2.10

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name="seveno_pyutil",
-    version='0.2.9',
+    version='0.2.10',
     license='MIT',
     description="Various unsorted Python utilities",
     long_description='%s\n%s' % (
