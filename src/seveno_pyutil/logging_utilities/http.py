75d1d7176168c2472b2a19b889f7871cd7ee11a5
Tomislav Adamic <tomislav.adamic@gmail.com>
2019-04-01 10:52:14 +0200

    fix: http logging utilites was forcing dict on JSON payloads
    This broke too many things, we should just log whatever we get and let
    others force / validate received data.

--- src/seveno_pyutil/logging_utilities/http.py
+++ src/seveno_pyutil/logging_utilities/http.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 def _colored_json(data):
     try:
-        if isinstance(data, dict):
+        if isinstance(data, (dict, list)):
             json_str = json.dumps(data)
         else:
             json_str = None
@@ -25,8 +25,8 @@ def _colored_json(data):
 
     return pygments.highlight(
         json_str,
-        pygments.lexers.get_lexer_for_mimetype('application/json'),
-        Terminal256Formatter(style='monokai')
+        pygments.lexers.get_lexer_for_mimetype("application/json"),
+        Terminal256Formatter(style="monokai"),
     )
 
 
@@ -43,35 +43,32 @@ def log_http_request(request, colorized=False):
     # Requests received by local Flask will have "path" and "url".
     # Requests outgoing from local Flask to some other service will have only
     # "url".
-    destination = (
-        getattr(request, "path", None) or getattr(request, "url", None)
-    )
+    destination = getattr(request, "path", None) or getattr(request, "url", None)
 
     logger.info("Started HTTP request %s: %s", request.method, destination)
 
     headers = [(k.strip(), v.strip()) for k, v in request.headers]
 
     logger.debug(
-        "HTTP request headers: %s", (
-            _colored_json(dict(headers))
-            if colorized else json.dumps(dict(headers))
-        ).strip()
+        "HTTP request headers: %s",
+        (
+            _colored_json(dict(headers)) if colorized else json.dumps(dict(headers))
+        ).strip(),
     )
 
     if (
         getattr(request, "is_json", None)
-        or request.headers.get('Content-Type', None) == 'application/json'
+        or request.headers.get("Content-Type", None) == "application/json"
     ):
         logger.info(
             "HTTP request payload: %s",
-            _colored_json(request.json).strip() if colorized
-            else str(request.json).strip()
+            _colored_json(request.json).strip()
+            if colorized
+            else str(request.json).strip(),
         )
 
 
-def log_http_response(
-    for_request, response, colorized=False, response_metrics=None
-):
+def log_http_response(for_request, response, colorized=False, response_metrics=None):
     """
     Log some stuff from HTTP request.
 
@@ -81,54 +78,57 @@ def log_http_response(
         Convert into `logging.Filter`
     """
 
-    headers = [(k.strip(), v.strip()) for k, v in response.headers]
+    try:
 
-    logger.debug(
-        "HTTP response headers: %s", (
-            _colored_json(dict(headers))
-            if colorized else json.dumps(dict(headers))
-        ).strip()
-    )
+        headers = [(k.strip(), v.strip()) for k, v in response.headers]
 
-    if (
-        getattr(response, 'content_type', None) == 'application/json'
-        or response.headers.get('Content-Type', None) == 'application/json'
-    ):
-        payload = getattr(response, 'json', ''
-                         ) or response.data.decode('UTF-8')
-
-        if callable(payload):
-            payload = payload()
-
-        if isinstance(payload, str):
-            payload = json.loads(payload)
-
-        logger.info(
-            "HTTP response payload: %s", (
-                _colored_json(dict(payload))
-                if colorized else json.dumps(dict(payload))
-            ).strip()
-        )
-
-    if response_metrics:
-        logger.info(
-            "Completed %s %s %s %s", for_request.method,
-            getattr(for_request, "path", None)
-            or getattr(for_request, "url", None),
-            getattr(response, 'status', None)
-            or getattr(response, 'status_code', None),
+        logger.debug(
+            "HTTP response headers: %s",
             (
-                _colored_json(dict(response_metrics))
-                if colorized else json.dumps(dict(response_metrics))
-            ).strip()
+                _colored_json(dict(headers)) if colorized else json.dumps(dict(headers))
+            ).strip(),
         )
-    else:
-        logger.info(
-            "Completed %s %s %s", for_request.method,
-            getattr(for_request, "path", None)
-            or getattr(for_request, "url", None),
-            getattr(response, 'status', None)
-            or getattr(response, 'status_code', None)
-        )
+
+        if (
+            getattr(response, "content_type", None) == "application/json"
+            or response.headers.get("Content-Type", None) == "application/json"
+        ):
+            payload = getattr(response, "json", "") or response.data.decode("UTF-8")
+
+            if callable(payload):
+                payload = payload()
+
+            if isinstance(payload, str):
+                payload = json.loads(payload)
+
+            logger.info(
+                "HTTP response payload: %s",
+                (_colored_json(payload) if colorized else json.dumps(payload)).strip(),
+            )
+
+        if response_metrics:
+            logger.info(
+                "Completed %s %s %s %s",
+                for_request.method,
+                getattr(for_request, "path", None) or getattr(for_request, "url", None),
+                getattr(response, "status", None)
+                or getattr(response, "status_code", None),
+                (
+                    _colored_json(dict(response_metrics))
+                    if colorized
+                    else json.dumps(dict(response_metrics))
+                ).strip(),
+            )
+        else:
+            logger.info(
+                "Completed %s %s %s",
+                for_request.method,
+                getattr(for_request, "path", None) or getattr(for_request, "url", None),
+                getattr(response, "status", None)
+                or getattr(response, "status_code", None),
+            )
+
+    except Exception:
+        logger.warning("Failed to log HTTP response!", exc_info=True)
 
     return response
