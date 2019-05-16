e3e94c9710ccd14f531ede3241e00dca58da3ef7
Tomislav Adamic <tomislav.adamic@gmail.com>
2019-05-16 09:47:12 +0200

    Removed HTTP logging helpers and DynamicContextFilter
    - HTTP logging helpers were not universal enough so they don't belong here
    - DynamicContextFilter was built with few wrong assumptions that all failed in real world so we are not using it anywhere any more. Removed because both, it doesn't work in all cases and is obsoleted by our apps

--- src/seveno_pyutil/logging_utilities/__init__.py
+++ src/seveno_pyutil/logging_utilities/__init__.py
@@ -1,6 +1,3 @@
-from .dynamic_context_filter import (COLORED_FILELOG_PREFIX, COLORLESS_FILELOG_PREFIX,
-                                     RFC5424_PREFIX, DynamicContextFilter)
-from .http import log_http_request, log_http_response
 from .single_line_formatter import SingleLineColoredFormatter, SingleLineFormatter
 from .sql_filter import SQLFilter
 from .standard_metadata_filter import StandardMetadataFilter
